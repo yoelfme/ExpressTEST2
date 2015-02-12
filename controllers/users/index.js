5bdf3bb8492dea56084f68d393aebeccaf486a87
Yoel Monzón <yoelfme@hotmail.com>
2015-02-12 15:58:28 -0600

    [create] Form and function new user

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-app.get('/user/new', function(request, response) {
- 
-  response.render('new');
- 
+app.get('/new', function(req, res, next) {
+	res.render('new_user');
 });
+
+app.post('/',function(req, res, next) {
+	var body = req.body;
+
+	var newUser = new db.User({
+		name: body.name,
+		isMale: u.isMale==="on" ? true : false
+	});
+
+	newUser.save(function(error, user) {
+		if(error)
+			res.json(error);
+
+		res.redirect('/');
+	});
+})
